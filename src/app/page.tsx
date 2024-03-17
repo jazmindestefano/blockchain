3a3f5c972ac00131888fdce7792a4a51d01ed9be
jazmindestefano <jazmin.destefano@gm2dev.com>
2024-03-17 12:12:30 -0300

    feat: add retrieved function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom/client'
 import {
   http,
   Address,
@@ -27,56 +26,101 @@ const walletClient = createWalletClient({
 })
 
 export default function Home() {
-  const [account, setAccount] = useState<Address>()
-  const [hash, setHash] = useState<Hash>()
-  const [receipt, setReceipt] = useState<TransactionReceipt>()
+  const [account, setAccount] = useState<Address>();
+  const [hash, setHash] = useState<Hash>();
+  const [retrievedValue, setRetrievedValue] = useState<BigInt>();
+  const [storeValue, setStoreValue] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
 
   const connect = async () => {
-    const [address] = await walletClient.requestAddresses()
-    setAccount(address)
-  }
+    const [address] = await walletClient.requestAddresses();
+    setAccount(address);
+    console.log({ address });
+  };
 
-  const mint = async () => {
-    if (!account) return
-    console.log("Validating transaction...");
-    const { request } = await publicClient.simulateContract({
-      ...storeContract,
-      account,
-      functionName: "store",
-      args: [BigInt(2)],
-    })
-    console.log("Sending transaction...");
-    const hash = await walletClient.writeContract(request)
-    setHash(hash)
-    console.log({ hash })
-  }
+  const store = async () => {
+    if (!account) return;
+
+    if (!storeValue.trim()) {
+      setError('Please provide a value for storing.');
+      return;
+    }
+
+    try {
+      console.log("Validating transaction...");
+      const { request } = await publicClient.simulateContract({
+        ...storeContract,
+        account,
+        functionName: "store",
+        args: [BigInt(storeValue)],
+      });
+      console.log("Sending transaction...");
+      const hash = await walletClient.writeContract(request);
+      setHash(hash);
+      console.log({ hash });
+    } catch (error) {
+      setError("Failed to store value.");
+    }
+  };
+
+  const retrieve = async () => {
+    if (!account) return;
+
+    try {
+      console.log("Reading contract...");
+      const retrieved = await publicClient.readContract({
+        ...storeContract,
+        account,
+        functionName: "retrieve",
+      });
+      setRetrievedValue(retrieved);
+      console.log({ retrievedValue });
+    } catch (error) {
+      setError("Failed to retrieve value.");
+    }
+  };
+
+  useEffect(() => {
+    retrieve();
+  }, [account, hash]);
 
   return (
-    <div className="min-h-screen bg-purple-500 flex items-center justify-center">
+    <div className="min-h-screen bg-gradient-to-br from-purple-500 to-pink-500 flex items-center justify-center">
       {account ? (
         <div className='flex flex-col items-center justify-center'>
           <div className="text-white">
-            Connected: {account}
+            Connected Account: {account}
           </div>
-          <button className="bg-white text-purple-500 px-4 py-2 rounded mt-4" onClick={mint}>
-            Mint
-          </button>
-          {receipt && (
-            <div className="text-white mt-4">
-              Receipt:{' '}
-              <pre>
-                <code>{stringify(receipt)}</code>
-              </pre>
-            </div>
-          )}
-           {hash && (
+          <div className='flex gap-4 items-center justify-center'>
+            <input
+              type="text"
+              value={storeValue}
+              onChange={(e) => setStoreValue(e.target.value)}
+              placeholder="Enter value"
+              className="bg-white text-purple-500 px-4 py-2 rounded mt-4"
+            />
+            <button className="bg-white text-purple-500 px-4 py-2 rounded mt-4" onClick={store}>
+              Store
+            </button>
+            <button className="bg-white text-purple-500 px-4 py-2 rounded mt-4" onClick={retrieve}>
+              Retrieve
+            </button>
+          </div>
+          {retrievedValue && (
             <div className="text-white flex flex-col items-center justify-center mt-4">
-              Transaction Hash:{' '}
-              <pre>
-                <code>{stringify(hash)}</code>
-              </pre>
+              <div className="bg-gradient-to-br from-purple-500 to-blue-500 text-white px-4 py-2 rounded">
+                Retrieved value: <span>{stringify(retrievedValue)}</span>
+              </div>
             </div>
           )}
+          {hash && (
+            <div className="text-white flex flex-col items-center justify-center mt-4">
+              <div className="bg-gradient-to-br from-purple-500 to-blue-500 text-white px-4 py-2 rounded">
+                Transaction Hash: <span>{hash}</span>
+              </div>
+            </div>
+          )}
+          {error && <div className="text-black mt-4">{error}</div>}
         </div>
       ) : (
         <button className="bg-white text-purple-500 px-4 py-2 rounded" onClick={connect}>
